7c622993581a176b69519024197fad3da16e8d89
Zach Burnaby <58493767+TheRedLancer@users.noreply.github.com>
2023-02-08 11:16:14 -0800

    add bash build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rootFolder="."
+buildFolder="$rootFolder/build"
+buildDist="$rootFolder/build/dist"
+
+npm i
+npx webpack --mode=production --config webpack.config.js
+
+if [ ! -d $buildFolder ]; then
+    mkdir $buildFolder
+fi
+if [ ! -d "$buildDist" ]; then
+    mkdir $buildDist
+fi
+cp $rootFolder/index.html $buildFolder
+cp $rootFolder/style.css $buildFolder
+cp $rootFolder/dist/* $buildDist
